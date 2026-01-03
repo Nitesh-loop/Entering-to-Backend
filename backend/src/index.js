54d2af01e9a440aec27bf08c5ebc1d1f7d476f36
Nitesh-loop <nitesh166k@gmail.com>
2026-01-03 20:09:03 +0530

    creating the model for auth API

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import app from "./app.js";
+
+dotenv.config({
+    path: './.env'
+});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.on("error", (error) => {
+            console.log("ERROR", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port :
+                ${process.env.PORT}`);
+        });
+
+        app.get("/", (req, res) => {
+            res.send("Hello World");
+        });
+
+
+    } catch (error) {
+        console.log("MonogoDB connection failed!!!", err);
+    }
+}
+
+startServer();
